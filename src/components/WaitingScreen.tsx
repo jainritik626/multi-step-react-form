a7ce95f5a12b5a95cde29d9487128e2a2f836fb3
jainritik626 <83767724+jainritik626@users.noreply.github.com>
2024-05-16 15:25:57 +0530

    Update WaitingScreen.tsx

--- src/components/WaitingScreen.tsx
+++ src/components/WaitingScreen.tsx
@@ -1,9 +1,17 @@
-const WaitingScreen = () => {
+const SuccessScreen = () => {
   return (
-    <div className="waiting-screen">
-      <div>Waiting for validation...</div>
+    <div className="success-screen">
+      <h2>Form Submitted Successfully!</h2>
+      <p>Your account has been created.</p>
+      <p>
+        Thank you for using our service! We'd love to hear your feedback. Please{" "}
+        <a href="https://mail.google.com/mail/?view=cm&fs=1&to=jainritik626@gmail.com">
+          Send a email
+        </a>
+        &nbsp;to give us your thoughts.
+      </p>
     </div>
   );
 };
 
-export default WaitingScreen;
+export default SuccessScreen;
