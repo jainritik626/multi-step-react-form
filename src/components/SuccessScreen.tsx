e43c0397b1f5955ecfa4f242a708b98960694045
jainritik626 <83767724+jainritik626@users.noreply.github.com>
2024-05-16 15:26:19 +0530

    Update SuccessScreen.tsx

--- src/components/SuccessScreen.tsx
+++ src/components/SuccessScreen.tsx
@@ -3,6 +3,13 @@ const SuccessScreen = () => {
     <div className="success-screen">
       <h2>Form Submitted Successfully!</h2>
       <p>Your account has been created.</p>
+      <p>
+        Thank you for using our service! We'd love to hear your feedback. Please{" "}
+        <a href="https://mail.google.com/mail/?view=cm&fs=1&to=jainritik626@gmail.com">
+          Send a email
+        </a>
+        &nbsp;to give us your thoughts.
+      </p>
     </div>
   );
 };
